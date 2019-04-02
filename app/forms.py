7d9087670fc29e827bfdb045fc1bc23e3f60da00
Tiffany Hudson <tiffany.hudson@mymona.uwi.edu>
2019-04-02 05:07:22 +0000

    Part 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
+from flask import render_template, request, redirect, url_for, flash 
+from flask_wtf import FlaskForm 
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, SelectField, TextAreaField, FileField
+from wtforms.validators import DataRequired, Email 
 
+
+class UploadForm(FlaskForm):
+    Description=TextAreaField('Description',validators=[DataRequired()]) 
+    Photo=FileField('Photo',validators=[FileRequired(),FileAllowed(['jpg','png','Images only!'])])
